489cfbc8a0a01a7286a48a8788920f7f2a76c4c0
navalraul <129669942+navalraul@users.noreply.github.com>
2023-07-10 19:10:49 +0530

    jfjsov

--- tata-cliq/src/components/Cart.jsx
+++ tata-cliq/src/components/Cart.jsx
@@ -38,18 +38,69 @@ const Cart = () => {
         <div className='details-info'>
             <div className='product-info'>
                 <div className="product-info-main">
-                    <div className='product-info-img'></div>
-                    <div className='product-info-det'></div>
+                    <div className='product-info-img'>
+                        <img src='https://img.tatacliq.com/images/i8/437Wx649H/MP000000015191655_437Wx649H_202211060047171.jpeg'/>
+                    </div>
+                    <div className='product-info-det'>
+                        <div className='product-info-det-fi'>
+                            <div className='product-info-det-fi-top'>
+                                <div className='product-info-det-fi-top-left'>
+                                    <p>U.S. Polo Assn. Dark Green Regular Fit Shirt</p>
+                                </div>
+                                <div className='product-info-det-fi-top-right'>
+                                    <img src='https://www.tatacliq.com/src/general/components/img/deliveryIcon.svg'/>
+                                    <p>Delivery by 16th Jul | <span>FREE </span></p>
+                                </div>
+                            </div>
+                            <div className='product-info-det-fi-me'>
+                                <p>₹2299.00</p>
+                            </div>
+                            <div className='product-info-det-fi-bot'>
+                                <div className='product-info-det-fi-bot-left'>
+                                    <p>Color: Dark Green</p>
+                                </div>
+                                <div className='product-info-det-fi-bot-right'>
+                                    <p>Size: XL</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='product-info-det-se'>
+                            <div className='product-info-det-se-left'>
+                                <p>Quantity: 1</p>
+                                <img src='https://www.tatacliq.com/src/general/components/img/down-arrow.svg'/>
+                            </div>
+                            <div className='product-info-det-se-right'>
+                                <img src='https://www.tatacliq.com/src/general/components/img/WL1.svg'/>
+                                <p>Save to wishlist</p>
+                                <p>Remove</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className='product-info-sec'>
                     <button>Continue Shopping</button>
                 </div>
             </div>
             <div className='product-total'>
-                <div className='product-total-top'>1</div>
-                <div className='product-total-bot'>2</div>
+                <div className='product-total-top'>
+                    <div className='product-total-top-left'>
+                        <p>Bag Total</p>
+                        <p>Shipping Charge</p>
+                        <p>Bag Subtotal</p>
+                    </div>
+                    <div className='product-total-top-right'>
+                        <p>₹2299.00</p>
+                        <span>FREE</span>
+                        <p>₹2299.00</p>
+                    </div>
+                </div>
+                <div className='product-total-bot'>
+                    <div className='product-total-bot-left'>1</div>
+                    <div className='product-total-bot-right'>2</div>
+                </div>
             </div>
         </div>
+        <div className='last'>2</div>
       </div>
     </div>
   )
